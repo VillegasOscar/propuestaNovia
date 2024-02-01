d48f431a80a7ec8d06b2c6a444d146f7bf322f2c
VillegasOscar <tuzitofire@hotmail.com>
2024-02-01 13:20:46 -0600

    f

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="css/style.css" rel="stylesheet" />
-		<title>♥ Kierrez Zer My Mobia ♥</title>
+		<title>Hola 🫦</title>
 	</head>
 
 	<body class="body">
